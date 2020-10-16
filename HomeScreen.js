3195bfbbf8327560c52057287368de74b751dc96
eunjeong09 <ejko0911@gmail.com>
2020-10-16 23:38:52 +0900

    reset project commit

---
+++ HomeScreen.js
@@ -0,0 +1,168 @@
+/**
+ * @summary 앱의 가장 메인이 되는 페이지
+ * 2020.10.06
+ */
+
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
+
+// const Main = () => {
+class HomeScreen extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      selectedColor: '#eee',
+    };
+  }
+
+  getToday = () => {
+    var date = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
+    return year + '.' + month + '.' + date; //yyyy.mm.dd
+  };
+
+  changeColor = (color) => {
+    this.setState({selectedColor: color});
+    //전역변수로 css 수정하기
+  };
+
+  render() {
+    return (
+      <View style={styles.wrap}>
+        <View style={styles.dateArea}>
+          <Text style={styles.date}>{this.getToday()}</Text>
+        </View>
+
+        <View style={styles.titleArea}>
+          <Text style={styles.title}>오늘 당신의 기분은 어떤 색인가요?</Text>
+        </View>
+        <View style={styles.todayColorWrap}>
+          <View
+            style={[
+              styles.todayColor,
+              {backgroundColor: this.state.selectedColor},
+            ]}
+          />
+        </View>
+        <View style={styles.colorListWrap}>
+          <TouchableOpacity
+            style={[styles.colorList, {backgroundColor: '#fb9da7'}]}
+            onPress={() => {
+              // alert(this.state.selectedColor);
+              {
+                this.changeColor('#fb9da7');
+              }
+            }}
+          />
+          <TouchableOpacity
+            style={[styles.colorList, {backgroundColor: '#fcccd4'}]}
+            onPress={() => {
+              // alert(this.state.selectedColor);
+              {
+                this.changeColor('#fcccd4');
+              }
+            }}
+          />
+          <TouchableOpacity
+            style={[styles.colorList, {backgroundColor: '#fbdea2'}]}
+            onPress={() => {
+              // alert(this.state.selectedColor);
+              {
+                this.changeColor('#fbdea2');
+              }
+            }}
+          />
+          <TouchableOpacity
+            style={[styles.colorList, {backgroundColor: '#f2e2c6'}]}
+            onPress={() => {
+              // alert(this.state.selectedColor);
+              {
+                this.changeColor('#f2e2c6');
+              }
+            }}
+          />
+          <TouchableOpacity
+            style={[styles.colorList, {backgroundColor: '#8eb695'}]}
+            onPress={() => {
+              // alert(this.state.selectedColor);
+              {
+                this.changeColor('#8eb695');
+              }
+            }}
+          />
+        </View>
+        <View style={styles.bottomArea}>
+          <Text>네비게이션 영역</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+// style
+const styles = StyleSheet.create({
+  wrap: {
+    flex: 1,
+    // justifyContent: 'space-around',
+  },
+  dateArea: {
+    // flex: 1,
+    backgroundColor: 'pink',
+    paddingTop: 70,
+    paddingBottom: 50,
+  },
+  date: {
+    fontSize: 30,
+    textAlign: 'center',
+  },
+  titleArea: {
+    // flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    marginBottom: 50,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 25,
+  },
+  todayColorWrap: {
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  todayColor: {
+    width: 250,
+    height: 250,
+    // backgroundColor: 'green',
+    borderColor: 'black',
+    borderWidth: 4,
+    borderRadius: 200,
+  },
+  colorListWrap: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  colorList: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    backgroundColor: 'black',
+  },
+  bottomArea: {
+    flex: 1,
+    backgroundColor: 'blue',
+    marginTop: 20,
+  },
+});
+
+export default HomeScreen;
