7c64304daedaca61eff9488adda67b8f63682ea4
eunjeong09 <ejko0911@gmail.com>
2020-10-22 23:08:00 +0900

    install command Line Tools.pkg

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ import HomeScreen from './HomeScreen';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => HomeScreen);
-// xcode Error
+// install command Line Tools.pkg
