10806a1fb5dd3a3d06e6d22ec63f728b1f628fb8
eunjeong09 <ejko0911@gmail.com>
2020-10-27 23:19:45 +0900

    use react navigation tab

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+import 'react-native-gesture-handler';
 /**
  * @format
  */
-// import 'react-native-gesture-handler';
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import HomeScreen from './HomeScreen';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => HomeScreen);
+AppRegistry.registerComponent(appName, () => App);
 // reinstall xcode,,,,,
