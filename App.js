51ca2fd9785274497a31697cc6a7e92a5f7824a9
eunjeong09 <ejko0911@gmail.com>
2020-11-04 23:14:16 +0900

    add navigationOptions

--- App.js
+++ App.js
@@ -7,14 +7,42 @@ import CalendarScreen from './CalendarScreen';
 import SettingScreen from './SettingScreen';
 
 const TabNavigator = createBottomTabNavigator({
-  Home: {
-    screen: HomeScreen,
+  // Home: {
+  //   screen: HomeScreen,
+  //   // navigationOptions:{
+
+  //   // }
+  // },
+  // Calendar: {
+  //   screen: CalendarScreen,
+  // },
+  // Setting: {
+  //   screen: SettingScreen,
+  // },
+  Home: HomeScreen,
+  Calendar:CalendarScreen,
+  Setting: SettingScreen,
+},
+{
+  navigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({ focused, horizontal, tintColor }) => {
+      const { routeName } = navigation.state;
+      let iconName;
+      if (routeName === 'Home') {
+        iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+      } else if (routeName === 'Setting') {
+        iconName = `ios-options${focused ? '' : '-outline'}`;
+      }
+
+      // You can return any component that you like here! We usually use an
+      // icon component from react-native-vector-icons
+      return <Ionicons name={iconName} size={horizontal ? 20 : 25} color={tintColor} />;
+    },
+  }),
+  tabBarOptions: {
+    activeTintColor: 'tomato',
+    inactiveTintColor: 'gray',
   },
-  Calendar: {
-    screen: CalendarScreen,
-  },
-  Setting: {
-    screen: SettingScreen,
-  },
-});
+}
+);
 export default createAppContainer(TabNavigator);
