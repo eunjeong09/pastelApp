d1110301148bf3c39b122889b6baa7c9555e3893
eunjeong09 <ejko0911@gmail.com>
2020-11-11 23:26:48 +0900

    add tabBarOptions

--- App.js
+++ App.js
@@ -1,41 +1,56 @@
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
-import {Icon} from 'react-native-vector-icons/FontAwesome';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import HomeScreen from './HomeScreen';
 import CalendarScreen from './CalendarScreen';
 import SettingScreen from './SettingScreen';
-
-const TabNavigator = createBottomTabNavigator({
-  Home: HomeScreen,
-  Calendar: CalendarScreen,
-  Setting: SettingScreen,
-});
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 // const TabNavigator = createBottomTabNavigator({
-//   Home: {
-//     screen: HomeScreen,
-//     path: './HomeScreen',
-//     navigationOptions: {
-//       title: 'Switch 1',
-//       tabBarLabel: 'Switch 1',
-//       tabBarIcon: ({tintColor, focused}) => (
-//         <Ionicons
-//           name={focused ? 'ios-home' : 'ios-home'}
-//           size={26}
-//           style={{color: tintColor}}
-//         />
-//       ),
-//     },
-//   },
-//   Calendar: {
-//     screen: CalendarScreen,
-//     path: './CalendarScreen',
-//   },
-//   Setting: {
-//     screen: SettingScreen,
-//   },
+//   Home: HomeScreen,
+//   Calendar: CalendarScreen,
+//   Setting: SettingScreen,
 // });
+
+const TabNavigator = createBottomTabNavigator({
+  Home: {
+    screen: HomeScreen,
+    path: './HomeScreen',
+    navigationOptions: {
+      title: 'Home',
+      tabBarLabel: 'Home',
+      navigationOptions: ({navigation}) => ({
+        tabBarIcon: ({focused, tintColor}) => {
+          const {routeName} = navigation.state;
+          if (routeName === 'Home') {
+            return <Icon name="rocket" size={30} color="#900" />;
+          } else if (routeName === 'CalendarScreen') {
+            return <Icon name="rocket" size={30} color="#900" />;
+          }
+          return null;
+        },
+      }),
+      tabBarOptions: {
+        showIcon: true,
+        showLabel: false,
+      },
+
+      // tabBarIcon: () =>(
+      //   <Icon
+      //     name="rocket" size={30} color="#900"
+      //   />
+      // )
+    },
+  },
+  Calendar: {
+    screen: CalendarScreen,
+    path: './CalendarScreen',
+  },
+  Setting: {
+    screen: SettingScreen,
+    path: './SettingScreen',
+  },
+});
 
 export default createAppContainer(TabNavigator);
 
